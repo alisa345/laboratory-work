63d36c950bf64f9d0f551c6dcd5927ebb87423b3
Alisa <43470252+alisa345@users.noreply.github.com>
2021-09-30 11:55:45 +0300

    lab4

---
+++ Лабораторная работа 4/exe4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            double a = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Введите b: ");
+            double b = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+            double sum = a + b;
+            double diff = Math.Abs(a - b);
+            double prod = a * b;
+            double quot = Math.Round(a / b, 4);
+            Console.WriteLine($"Сумма: {sum} \nРазность: {diff} \nПроизведение: {prod} \nЧастное: {quot}");
+            Console.ReadKey();
+        }
+    }
+}
